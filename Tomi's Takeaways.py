ccbce2c595e438e87bcfa1765504e56a0f90bd73
Crypt-cat <tomibrew806@gmail.com>
2026-04-29 13:54:05 +1200

    Main Routine Setup
    I set up the default window size by doing the maths for the midpoint of the user's screen, added a title, made it so you can't resize the window and more. I also made the buttons in the starting frame bigger and bolder, which I now need to put in the middle of the window. I also added the next frames.

--- Tomi's Takeaways.py
+++ Tomi's Takeaways.py
@@ -6,23 +6,36 @@ from tkinter import messagebox
 
 class OrderingSystem:
     def __init__(self, parent):
-        self.parent = parent
-        self.parent.title("Tomi's Takeaways")
-
         self.order_list = []
 
         self.start_frame = Frame(parent)
         self.start_frame.grid()
 
-        Button(self.start_frame, text="Food Menu", command=self.show_food_menu)
-        Button(self.start_frame, text="Combo Menu", command=self.show_combo_menu)
+        Button(self.start_frame, text="Food Menu", font=("TKDefault", 30, "bold"), command=self.show_food_menu).grid(columnspan=2, sticky="nesw")
+        Button(self.start_frame, text="Combo Menu", font=("TKDefault", 30, "bold"), command=self.show_combo_menu).grid(columnspan=2, sticky="nesw")
+
+        self.food_menu = Frame(parent)
+        Label(self.food_menu, text="Featured Items").grid()
+
+
+        self.combo_menu = Frame(parent)
+
     
     def show_food_menu(self):
+        self.start_frame.grid_forget()
+        self.food_menu.grid()
 
     
     def show_combo_menu(self):
+        self.start_frame.grid_forget()
+        self.combo_menu.grid()
+
 
 if __name__ == "__main__":
     root = Tk()
+    root.title("Tomi's Takeaways")
+    screen_middle = (root.winfo_screenwidth() / 2) - 250
+    root.geometry(f"500x{root.winfo_screenheight()}+{int(screen_middle)}+0")
+    root.resizable(False, False)
     gui = OrderingSystem(root)
-    root.mainloop()
+    root.mainloop() 
